852c3626ca18906fc735347b4dbc16bdfa1c2eed
Jose Martinez <jfmartin@us.ibm.com>
2015-07-09 13:28:48 -0400

    Landing page app

---
+++ app/docs.js
@@ -0,0 +1,44 @@
+API_DOCS={
+
+	"pages": [
+
+		{	"state": "api.RtcommVideo",
+			"category": "directive",
+			"name": "RtcommVideo",
+			"templateUrl": "templates/api/rtcomm-video.html"
+
+		},
+
+		{
+			"state": "api.RtcommChat",
+			"category": "directive",
+			"name":"RtcommChat",
+			"templateUrl" : "templates/api/rtcomm-chat.html"
+		},
+
+		{
+			"state": "api.RtcommService",
+			"category": "service",
+			"name": "RtcommService",
+			"templateUrl" : "templates/api/rtcomm-service.html"
+
+
+		},
+
+		{
+
+			"state": "api.RtcommConfig",
+			"category": "service",
+			"name": "RtcommConfig",
+			"templateUrl" : " templates/api/rtcomm-config.html"
+
+
+		}
+
+
+
+	]
+
+
+
+}
